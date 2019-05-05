cb699e4ce3f4d0e5749dddd068f0e9e76a491536
Hai Nguyen <quanghai.ng1512@gmail.com>
2019-05-05 21:08:06 +0700

    Seeding Done

--- DotnetGraphQL.Data/Seed/DataSeeding.cs
+++ DotnetGraphQL.Data/Seed/DataSeeding.cs
@@ -12,11 +12,11 @@ namespace DotnetGraphQL.Data.Seed
         {
             ctx.Logger.LogInformation("Seeding database");
 
-            List<Department> depts = new List<Department>
+            var depts = new List<Department>
             {
                 new Department { DeptName = "R&D Squad" },
                 new Department { DeptName = "Outsourcing"},
-                new Department { DeptName = "Digital"}
+                new Department { DeptName = "Freelance"}
             };
 
             if (!ctx.Departments.Any())
@@ -26,13 +26,125 @@ namespace DotnetGraphQL.Data.Seed
                 ctx.SaveChanges();
             }
 
+            var tasks = new List<Task>
+            {
+                new Task { TaskName = "Deploy service" },
+                new Task { TaskName = "Building GUI" },
+                new Task { TaskName = "Training model" },
+                new Task { TaskName = "Testing feature" },
+                new Task { TaskName = "Research framework" },
+            };
+
+            if (!ctx.Tasks.Any())
+            {
+                ctx.Logger.LogInformation("Seeding Tasks");
+                ctx.Tasks.AddRange(tasks);
+                ctx.SaveChanges();
+            }
+
             if (!ctx.Employees.Any())
             {
                 ctx.Logger.LogInformation("Seeding Employees");
                 var emps = new List<Employee>
                 {
-                    new Employee  { EmpName = "John Cena", Dob = new DateTime(1990, 1, 1), Salary = 2000 }
+                    new Employee
+                    {
+                        EmpName = "Black Widow",
+                        Dob = new DateTime(1990, 1, 1),
+                        Salary = 2000, Department = depts[0],
+                        Assignments = new List<Assignment>
+                        {
+                            new Assignment { AssignedTask = tasks[0] },
+                            new Assignment { AssignedTask = tasks[2] },
+                            new Assignment { AssignedTask = tasks[1] },
+                        }
+                    },
+                    new Employee
+                    {
+                        EmpName = "Thor Odinson",
+                        Dob = new DateTime(1991, 6, 22),
+                        Salary = 1500, Department = depts[2],
+                        Assignments = new List<Assignment>
+                        {
+                            new Assignment { AssignedTask = tasks[2] },
+                            new Assignment { AssignedTask = tasks[3] },
+                            new Assignment { AssignedTask = tasks[4] },
+                        }
+                    },
+                    new Employee
+                    {
+                        EmpName = "Iron Man",
+                        Dob = new DateTime(1993, 2, 1),
+                        Salary = 1200,
+                        Department = depts[1],
+                        Assignments = new List<Assignment>
+                        {
+                            new Assignment { AssignedTask = tasks[0] },
+                            new Assignment { AssignedTask = tasks[1] },
+                            new Assignment { AssignedTask = tasks[4] }
+                        }
+                    },
+                    new Employee
+                    {
+                        EmpName = "Cap America",
+                        Dob = new DateTime(1991, 9, 29),
+                        Salary = 2200, Department = depts[1],
+                        Assignments = new List<Assignment>
+                        {
+                            new Assignment { AssignedTask = tasks[2] },
+                            new Assignment { AssignedTask = tasks[3] }
+                        }
+                    },
+                    new Employee
+                    { 
+                        EmpName = "Rocket Racoon",
+                        Dob = new DateTime(1986, 3, 21),
+                        Salary = 1100,
+                        Department = depts[0],
+                        Assignments = new List<Assignment>
+                        {
+                            new Assignment { AssignedTask = tasks[3] },
+                            new Assignment { AssignedTask = tasks[4] }
+                        }
+                    },
+                    new Employee
+                    { 
+                        EmpName = "Hawk Eye",
+                        Dob = new DateTime(1985, 4, 14),
+                        Salary = 2000,
+                        Department = depts[0],
+                        Assignments = new List<Assignment>
+                        {
+                            new Assignment { AssignedTask = tasks[1] }
+                        }
+                    },
+                    new Employee
+                    { 
+                        EmpName = "The Hulk",
+                        Dob = new DateTime(1979, 5, 9),
+                        Salary = 1900,
+                        Department = depts[2],
+                        Assignments = new List<Assignment>
+                        {
+                            new Assignment { AssignedTask = tasks[3] },
+                            new Assignment { AssignedTask = tasks[2] }
+                        }
+                    },
+                    new Employee
+                    { 
+                        EmpName = "Ant-man",
+                        Dob = new DateTime(1988, 7, 3),
+                        Salary = 1500,
+                        Department = depts[1],
+                        Assignments = new List<Assignment>
+                        {
+                            new Assignment { AssignedTask = tasks[3] },
+                            new Assignment { AssignedTask = tasks[4] }
+                        }
+                    }
                 };
+                ctx.Employees.AddRange(emps);
+                ctx.SaveChanges();
             }
         }
     }

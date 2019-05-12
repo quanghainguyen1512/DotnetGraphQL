f8ac0858055e474aa41afab8a9f747563ba7f7d6
Hai Nguyen <quanghai.ng1512@gmail.com>
2019-05-13 05:39:24 +0700

    New Migrations

--- DotnetGraphQL.Data/Migrations/20190512171829_ReInit.Designer.cs
+++ DotnetGraphQL.Data/Migrations/20190512171829_ReInit.Designer.cs
@@ -4,14 +4,16 @@ using DotnetGraphQL.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DotnetGraphQL.Data.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20190512171829_ReInit")]
+    partial class ReInit
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ DotnetGraphQL.Data/Migrations/20190512171829_ReInit.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DotnetGraphQL.Data.Migrations
+{
+    public partial class ReInit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Assignments_Employees_EmployeeId",
+                table: "Assignments");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Assignments",
+                table: "Assignments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Assignments_EmployeeId",
+                table: "Assignments");
+
+            migrationBuilder.DropColumn(
+                name: "EmpId",
+                table: "Assignments");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "EmployeeId",
+                table: "Assignments",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldNullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Assignments",
+                table: "Assignments",
+                columns: new[] { "EmployeeId", "TaskId" });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Assignments_Employees_EmployeeId",
+                table: "Assignments",
+                column: "EmployeeId",
+                principalTable: "Employees",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Assignments_Employees_EmployeeId",
+                table: "Assignments");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Assignments",
+                table: "Assignments");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "EmployeeId",
+                table: "Assignments",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AddColumn<int>(
+                name: "EmpId",
+                table: "Assignments",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Assignments",
+                table: "Assignments",
+                columns: new[] { "EmpId", "TaskId" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Assignments_EmployeeId",
+                table: "Assignments",
+                column: "EmployeeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Assignments_Employees_EmployeeId",
+                table: "Assignments",
+                column: "EmployeeId",
+                principalTable: "Employees",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}

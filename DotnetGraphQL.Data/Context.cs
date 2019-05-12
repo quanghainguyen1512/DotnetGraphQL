20cdcecbf5a57cbd763f4aff86dec93e8632cdee
Hai Nguyen <quanghai.ng1512@gmail.com>
2019-05-13 05:40:03 +0700

    Fix context and seeding

--- DotnetGraphQL.Data/Context.cs
+++ DotnetGraphQL.Data/Context.cs
@@ -1,37 +1,35 @@
 using Microsoft.EntityFrameworkCore;
 using DotnetGraphQL.Core.Entities;
 using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
 
 namespace DotnetGraphQL.Data
 {
     public class Context : DbContext
     {
         public readonly ILogger Logger;
-        private bool _migrations; 
+        // private bool _migrations; 
 
         public Context()
         {
-            _migrations = true;
+            // _migrations = true;
         }
 
         public Context(DbContextOptions options, ILogger<Context> logger) : base(options) 
         {
             Logger = logger;
+            Database.Migrate();
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) 
         {
-            if (_migrations)
-            {
-                optionsBuilder.UseSqlServer("Server=localhost;Database=emapi;User Id=SA;Password=Haideptrai1;");
-            }
-
+            optionsBuilder.EnableSensitiveDataLogging();
             base.OnConfiguring(optionsBuilder);
         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Assignment>().HasKey(a => new { a.EmpId, a.TaskId });
+            modelBuilder.Entity<Assignment>().HasKey(a => new { a.EmployeeId, a.TaskId });
         }
 
         public virtual DbSet<Employee> Employees { get; set; }
